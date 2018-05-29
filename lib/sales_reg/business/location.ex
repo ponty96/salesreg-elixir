8066cee38d4d8eb7634e7f8e056f1b67af750a01
Ayomide Aregbede <ayo.aregbede@gmail.com>
2018-05-29 21:33:51 +0100

    create company branch with location when user registers a company

---
+++ lib/sales_reg/business/location.ex
@@ -0,0 +1,28 @@
+defmodule SalesReg.Business.Location do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  @primary_key {:id, :binary_id, autogenerate: true}
+  @foreign_key_type :binary_id
+  schema "locations" do
+    field(:city, :string)
+    field(:country, :string)
+    field(:lat, :string)
+    field(:long, :string)
+    field(:state, :string)
+    field(:street1, :string)
+    field(:street2, :string)
+    belongs_to(:branch, Branch)
+
+    timestamps()
+  end
+
+  @required_fields [:street1, :city, :state, :country]
+  @fields [:street2, :lat, :long]
+  @doc false
+  def changeset(location, attrs) do
+    location
+    |> cast(attrs, @required_fields ++ @fields)
+    |> validate_required(@required_fields)
+  end
+end
---
+++ priv/repo/migrations/20180529173359_create_locations.exs
@@ -0,0 +1,21 @@
+defmodule SalesReg.Repo.Migrations.CreateLocations do
+  use Ecto.Migration
+
+  def change do
+    create table(:locations, primary_key: false) do
+      add :id, :binary_id, primary_key: true
+      add :lat, :string
+      add :long, :string
+      add :street1, :string
+      add :street2, :string
+      add :city, :string
+      add :state, :string
+      add :country, :string
+      add :branch_id, references(:branches, on_delete: :nothing, type: :binary_id)
+
+      timestamps()
+    end
+
+    create index(:locations, [:branch_id])
+  end
+end
