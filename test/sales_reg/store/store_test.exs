91e257498f7efb86a49bced383a83faba0ddd29f
Ayomide Aregbede <ayo.aregbede@gmail.com>
2018-06-01 02:25:36 +0100

    service CRUD and graphql object, query and mutations

--- test/sales_reg/store/store_test.exs
+++ test/sales_reg/store/store_test.exs
@@ -74,4 +74,68 @@ defmodule SalesReg.StoreTest do
       assert %Ecto.Changeset{} = Store.change_product(product)
     end
   end
+
+  describe "services" do
+    alias SalesReg.Store.Service
+
+    @valid_attrs %{description: "some description", name: "some name", price: "some price"}
+    @update_attrs %{description: "some updated description", name: "some updated name", price: "some updated price"}
+    @invalid_attrs %{description: nil, name: nil, price: nil}
+
+    def service_fixture(attrs \\ %{}) do
+      {:ok, service} =
+        attrs
+        |> Enum.into(@valid_attrs)
+        |> Store.create_service()
+
+      service
+    end
+
+    test "list_services/0 returns all services" do
+      service = service_fixture()
+      assert Store.list_services() == [service]
+    end
+
+    test "get_service!/1 returns the service with given id" do
+      service = service_fixture()
+      assert Store.get_service!(service.id) == service
+    end
+
+    test "create_service/1 with valid data creates a service" do
+      assert {:ok, %Service{} = service} = Store.create_service(@valid_attrs)
+      assert service.description == "some description"
+      assert service.name == "some name"
+      assert service.price == "some price"
+    end
+
+    test "create_service/1 with invalid data returns error changeset" do
+      assert {:error, %Ecto.Changeset{}} = Store.create_service(@invalid_attrs)
+    end
+
+    test "update_service/2 with valid data updates the service" do
+      service = service_fixture()
+      assert {:ok, service} = Store.update_service(service, @update_attrs)
+      assert %Service{} = service
+      assert service.description == "some updated description"
+      assert service.name == "some updated name"
+      assert service.price == "some updated price"
+    end
+
+    test "update_service/2 with invalid data returns error changeset" do
+      service = service_fixture()
+      assert {:error, %Ecto.Changeset{}} = Store.update_service(service, @invalid_attrs)
+      assert service == Store.get_service!(service.id)
+    end
+
+    test "delete_service/1 deletes the service" do
+      service = service_fixture()
+      assert {:ok, %Service{}} = Store.delete_service(service)
+      assert_raise Ecto.NoResultsError, fn -> Store.get_service!(service.id) end
+    end
+
+    test "change_service/1 returns a service changeset" do
+      service = service_fixture()
+      assert %Ecto.Changeset{} = Store.change_service(service)
+    end
+  end
 end
