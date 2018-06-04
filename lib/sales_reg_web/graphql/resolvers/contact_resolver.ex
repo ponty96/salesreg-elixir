74bb450c5f99b93fbe41e7e2ddfbd1022dedeb29
Badmos Opeyemi Philip <opeyemi.badmos@gmail.com>
2018-06-04 22:44:10 +0100

    change response type in context get_schema

--- lib/sales_reg_web/graphql/resolvers/contact_resolver.ex
+++ lib/sales_reg_web/graphql/resolvers/contact_resolver.ex
@@ -6,19 +6,16 @@ defmodule SalesRegWeb.GraphQL.Resolvers.ContactResolver do
   end
 
   def update_contact(%{contact: params, contact_id: contact_id}, _res) do
-    {:ok, contact} = Business.get_contact(contact_id)
-
-    contact
+    Business.get_contact(contact_id)
     |> Business.update_contact(params)
   end
 
-  def list_company_contacts(%{company_id: company_id} = another, _res) do
+  def list_company_contacts(%{company_id: company_id}, _res) do
     Business.list_company_contacts(company_id)
   end
 
   def delete_contact(%{contact_id: contact_id}, _res) do
-    {:ok, contact} = Business.get_contact(contact_id)
-
-    Business.delete_contact(contact)
+    Business.get_contact(contact_id)
+    |> Business.delete_contact()
   end
 end
