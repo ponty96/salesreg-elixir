d2e76395710c481ca97dcf20136a340a10a85c6c
Badmos Opeyemi Philip <opeyemi.badmos@gmail.com>
2018-07-19 18:10:07 +0100

    create update graphql service for user and company and create image upload service

--- config/config.exs
+++ config/config.exs
@@ -34,6 +34,22 @@ config :guardian, Guardian.DB,
   sweep_interval: 60,
   ttl: {15, :days}
 
+  config :ex_aws,
+    access_key_id: [{:system, "AWS_ACCESS_KEY_ID"}, :instance_role],
+    secret_access_key: [{:system, "AWS_SECRET_ACCESS_KEY"}, :instance_role]
+
+  config :ex_aws, :hackney_opts,
+    follow_redirect: true,
+    recv_timeout: 30_000
+
+  config :ex_aws,
+    region: "us-west-2"
+
+  config :ex_aws, :retries,
+    max_attempts: 10,
+    base_backoff_in_ms: 10,
+    max_backoff_in_ms: 10_000
+    
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{Mix.env()}.exs"
