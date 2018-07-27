3113084d3d07ce8cedbbe7a23ad25ebfb002681b
Badmos Opeyemi Philip <opeyemi.badmos@gmail.com>
2018-07-27 18:44:26 +0100

    merge edit_product_schema to JIRA-ISSUE-SW-32

--- priv/repo/migrations/20180607124247_create_phones.exs
+++ priv/repo/migrations/20180607124247_create_phones.exs
@@ -7,11 +7,10 @@ defmodule SalesReg.Repo.Migrations.CreatePhones do
       add(:type, :string)
       add(:number, :string)
 
-      add(:customer_id, references(:customers, on_delete: :nothing, type: :binary_id))
-
+      add :customer_id, references(:customers, on_delete: :nothing, type: :binary_id)
+      add :user_id, references(:users, on_delete: :nothing, null: false, type: :binary_id)
       timestamps()
     end
-
-    create(index(:phones, [:customer_id]))
+    create index(:phones, [:customer_id, :user_id])
   end
 end
