1578afe2c1c6fbb7b5473d093f826c381cd3a57b
Ayomide Aregbede <ayo.aregbede@gmail.com>
2018-08-06 11:26:29 +0100

    fix config

--- config/dev.exs
+++ config/dev.exs
@@ -7,7 +7,7 @@ use Mix.Config
 # watchers to your application. For example, we use it
 # with brunch.io to recompile .js and .css sources.
 config :sales_reg, SalesRegWeb.Endpoint,
-  url: [host: "localhost", port: 5000],
+  http: [port: 5000],
   debug_errors: true,
   code_reloader: true,
   check_origin: false,
