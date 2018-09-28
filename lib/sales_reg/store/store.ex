fce6c62360b46560d96654affa26722169cae9f2
Ayomide Aregbede <ayo.aregbede@gmail.com>
2018-09-28 10:14:42 +0100

    add order and store data loader source

--- lib/sales_reg/store/store.ex
+++ lib/sales_reg/store/store.ex
@@ -2,12 +2,19 @@ defmodule SalesReg.Store do
   @moduledoc """
   The Store context.
   """
-
-  import Ecto.Query, warn: false
   use SalesRegWeb, :context
+  alias Dataloader.Ecto, as: DataloaderEcto
 
   use SalesReg.Context, [
     Product,
     Service
   ]
+
+  def data do
+    DataloaderEcto.new(Repo, query: &query/2)
+  end
+
+  def query(queryable, _) do
+    queryable
+  end
 end
