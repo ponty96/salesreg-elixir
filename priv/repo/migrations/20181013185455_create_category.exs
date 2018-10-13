dda5a2c6e66e4dcbe6a4c69cc9a452d6fd28e687
Ayomide Aregbede <ayo.aregbede@gmail.com>
2018-10-13 20:05:44 +0100

    update category migration

--- priv/repo/migrations/20181013185455_create_category.exs
+++ priv/repo/migrations/20181013185455_create_category.exs
@@ -6,6 +6,11 @@ defmodule SalesReg.Repo.Migrations.CreateCategory do
       add(:id, :binary_id, primary_key: true)
       add(:title, :string)
       add(:description, :string)
+
+      add(:company_id, references(:companies, on_delete: :nothing, type: :binary_id))
+      add(:user_id, references(:users, on_delete: :nothing, type: :binary_id))
+
+      timestamps()
     end
 
     create table(:products_categories, primary_key: false) do
@@ -17,5 +22,8 @@ defmodule SalesReg.Repo.Migrations.CreateCategory do
       add :services_id, references(:services, type: :binary_id)
       add :category_id, references(:categories, type: :binary_id)
     end
+
+    create(index(:categories, [:company_id]))
+    create(index(:categories, [:user_id]))
   end
 end
