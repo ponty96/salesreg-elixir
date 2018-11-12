bad25f60fdc2833e760a23a9229f499328851e4e
Ayomide Aregbede <ayo.aregbede@gmail.com>
2018-11-12 19:33:05 +0100

    fix bugs with invoice and receipt schema and migration

--- lib/sales_reg/order/invoice.ex
+++ lib/sales_reg/order/invoice.ex
@@ -2,13 +2,15 @@ defmodule SalesReg.Order.Invoice do
   use Ecto.Schema
   import Ecto.Changeset
 
+  @primary_key {:id, :binary_id, autogenerate: true}
+  @foreign_key_type :binary_id
   schema "invoices" do
     field(:due_date, :string)
 
     has_many(:receipts, SalesReg.Order.Receipt)
     belongs_to(:sale, SalesReg.Order.Sale)
     belongs_to(:user, SalesReg.Accounts.User)
-    belongs_to(:company, SalesReg.Business.Companies)
+    belongs_to(:company, SalesReg.Business.Company)
 
     timestamps()
   end
