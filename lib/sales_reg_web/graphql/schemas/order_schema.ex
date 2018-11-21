732bb77b8eafbd512d53fa13083638175e78cbb7
Badmos Opeyemi Philip <opeyemi.badmos@gmail.com>
2018-11-21 18:56:55 +0100

    create receipt graphql schema

--- lib/sales_reg_web/graphql/schemas/order_schema.ex
+++ lib/sales_reg_web/graphql/schemas/order_schema.ex
@@ -49,6 +49,16 @@ defmodule SalesRegWeb.GraphQL.Schemas.OrderSchema do
 
       resolve(&OrderResolver.update_invoice_due_date/2)
     end
+
+    ### Receipt mutations
+    @desc """
+    upsert a receipt
+    """
+    field :upsert_receipt, :mutation_response do
+      arg(:receipt, non_null(:receipt_input))
+
+      resolve(&OrderResolver.upsert_receipt/2)
+    end
   end
 
   ### QUERIES
