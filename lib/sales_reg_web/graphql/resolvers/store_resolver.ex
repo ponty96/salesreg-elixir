b86a8fff7dda25232613f10000da7407f071a05c
Ayomide Aregbede <ayo.aregbede@gmail.com>
2018-12-04 16:42:59 +0100

    rearrange functions in StoreResolver module

--- lib/sales_reg_web/graphql/resolvers/store_resolver.ex
+++ lib/sales_reg_web/graphql/resolvers/store_resolver.ex
@@ -11,91 +11,6 @@ defmodule SalesRegWeb.GraphQL.Resolvers.StoreResolver do
     Store.add_service(params)
   end
 
-  def list_company_services(%{company_id: company_id} = args, _res) do
-    {:ok, services} = Store.list_company_services(company_id)
-
-    services
-    |> Absinthe.Relay.Connection.from_list(pagination_args(args))
-  end
-
-  def delete_service(%{service_id: service_id}, _res) do
-    service = Store.get_service(service_id)
-    Store.delete_service(service)
-  end
-
-  def delete_service(%{service_id: service_id}, _res) do
-    service = Store.get_service(service_id)
-    Store.delete_service(service)
-  end
-
-  def upsert_product(%{product: params, product_id: id}, _res) do
-    Store.update_product(id, params)
-  end
-
-  def upsert_product(%{product: params}, _res) do
-    Store.add_product(params)
-  end
-
-  def list_company_products(%{company_id: company_id} = args, _res) do
-    {:ok, products} = Store.list_company_products(company_id)
-
-    products
-    |> Absinthe.Relay.Connection.from_list(pagination_args(args))
-  end
-
-  def search_products_services_by_name(%{query: query}, _res) do
-    products_and_services = Store.load_prod_and_serv(query)
-
-    {:ok, products_and_services}
-  end
-
-  def delete_product(%{product_id: product_id}, _res) do
-    product = Store.get_product(product_id)
-    Store.delete_product(product)
-  end
-
-  def delete_product(%{product_id: product_id}, _res) do
-    product = Store.get_product(product_id)
-    Store.delete_product(product)
-  end
-
-  def list_featured_items(%{company_id: company_id}, _res) do
-    Store.list_featured_items(company_id)
-  end
-
-  def list_top_rated_items(%{company_id: company_id}, _res) do
-    Store.list_top_rated_items(company_id)
-  end
-
-  # category
-  def upsert_category(%{category: params, category_id: id}, _res) do
-    Store.get_category(id)
-    |> Store.update_category(params)
-  end
-
-  def upsert_category(%{category: params}, _res) do
-    Store.add_category(params)
-  end
-
-  def list_company_categories(%{company_id: company_id} = args, _res) do
-    {:ok, categories} = Store.list_company_categorys(company_id)
-
-    categories
-    |> Absinthe.Relay.Connection.from_list(pagination_args(args))
-  end
-
-  # tag
-  def list_company_tags(%{company_id: id} = args, _res) do
-    {:ok, tags} = Business.list_company_tags(id)
-
-    tags
-    |> Absinthe.Relay.Connection.from_list(pagination_args(args))
-  end
-
-  defp pagination_args(args) do
-    Map.take(args, [:first, :after, :last, :before])
-  end
-
   def create_product(%{params: params}, _res) do
     Store.create_product(params)
   end
@@ -109,6 +24,55 @@ defmodule SalesRegWeb.GraphQL.Resolvers.StoreResolver do
     Store.update_product_group_options(params)
   end
 
+  # category
+  def upsert_category(%{category: params, category_id: id}, _res) do
+    Store.get_category(id)
+    |> Store.update_category(params)
+  end
+
+  def upsert_category(%{category: params}, _res) do
+    Store.add_category(params)
+  end
+
+  def delete_service(%{service_id: service_id}, _res) do
+    service = Store.get_service(service_id)
+    Store.delete_service(service)
+  end
+
+  def delete_product(%{product_id: product_id}, _res) do
+    product = Store.get_product(product_id)
+    Store.delete_product(product)
+  end
+
+  def list_company_services(%{company_id: company_id} = args, _res) do
+    {:ok, services} = Store.list_company_services(company_id)
+
+    services
+    |> Absinthe.Relay.Connection.from_list(pagination_args(args))
+  end
+
+  def list_company_products(%{company_id: company_id} = args, _res) do
+    {:ok, products} = Store.list_company_products(company_id)
+
+    products
+    |> Absinthe.Relay.Connection.from_list(pagination_args(args))
+  end
+
+  def list_featured_items(%{company_id: company_id}, _res) do
+    Store.list_featured_items(company_id)
+  end
+
+  def list_top_rated_items(%{company_id: company_id}, _res) do
+    Store.list_top_rated_items(company_id)
+  end
+
+  def list_company_categories(%{company_id: company_id} = args, _res) do
+    {:ok, categories} = Store.list_company_categorys(company_id)
+
+    categories
+    |> Absinthe.Relay.Connection.from_list(pagination_args(args))
+  end
+
   def search_product_groups_by_title(%{company_id: company_id, query: query}, _res) do
     {:ok, SalesReg.Context.search_schema_by_field(ProductGroup, {query, company_id}, :title)}
   end
@@ -119,5 +83,13 @@ defmodule SalesRegWeb.GraphQL.Resolvers.StoreResolver do
 
   def search_categories_by_title(%{company_id: company_id, query: query}, _res) do
     {:ok, SalesReg.Context.search_schema_by_field(Category, {query, company_id}, :title)}
+  end
+
+  def search_products_services_by_name(%{query: query}, _res) do
+    {:ok, Store.load_prod_and_serv(query)}
+  end
+
+  defp pagination_args(args) do
+    Map.take(args, [:first, :after, :last, :before])
   end
 end
