1ac680e0ef0f439e7011d47ae41db61be8af7681
Ayomide Aregbede <ayo.aregbede@gmail.com>
2018-12-04 16:40:13 +0100

    add delete changeset to option and remove change options delete relationship to option_values

--- priv/repo/migrations/20181101113536_create_variant_option_values.exs
+++ priv/repo/migrations/20181101113536_create_variant_option_values.exs
@@ -6,7 +6,7 @@ defmodule SalesReg.Repo.Migrations.CreateOptionValues do
       add(:id, :binary_id, primary_key: true)
       add(:name, :string)
       add(:company_id, references(:companies, on_delete: :nothing, type: :binary_id))
-      add(:option_id, references(:options, on_delete: :delete_all, type: :binary_id))
+      add(:option_id, references(:options, on_delete: :nothing, type: :binary_id))
 
       add(:product_id, references(:products, type: :binary_id, on_delete: :delete_all))
 
