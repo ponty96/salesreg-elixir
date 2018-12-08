156cf6d7e4f7be8280c988a4986bd5db904279b9
kike907 <kikelomofunso@gmail.com>
2018-12-08 11:58:05 +0100

    create migration files for template and company_template

--- priv/repo/migrations/20181206033556_create_template.exs
+++ priv/repo/migrations/20181206033556_create_template.exs
@@ -2,6 +2,19 @@ defmodule SalesReg.Repo.Migrations.CreateTemplate do
   use Ecto.Migration
 
   def change do
+    create table(:template, primary_key: false) do
+      add(:id, :binary_id, primary_key: true)
+      add(:title, :string)
+      add(:slug, :string)
+      add(:featured_image, :string)
 
+      add(:user_id, references(:users, type: :binary_id))
+      add(:company_id, references(:companies, type: :binary_id))
+
+      timestamps()
+    end
+    
+    create(index(:invoices, [:user_id], on_delete: :nothing, type: :binary_id))
+    create(index(:invoices, [:company_id], on_delete: :nothing, type: :binary_id))
   end
 end
--- priv/repo/migrations/20181206033706_create_company_template.exs
+++ priv/repo/migrations/20181206033706_create_company_template.exs
@@ -2,6 +2,19 @@ defmodule SalesReg.Repo.Migrations.CreateCompanyTemplate do
   use Ecto.Migration
 
   def change do
+    create table(:template, primary_key: false) do
+      add(:id, :binary_id, primary_key: true)
+      add(:status, :string)
+    
+      add(:template_id, references(:templates, type: :binary_id))
+      add(:user_id, references(:users, type: :binary_id))
+      add(:company_id, references(:companies, type: :binary_id))
 
+      timestamps()
+    end
+
+    create(index(:invoices, [:template_id], on_delete: :nothing, type: :binary_id))
+    create(index(:invoices, [:user_id], on_delete: :nothing, type: :binary_id))
+    create(index(:invoices, [:company_id], on_delete: :nothing, type: :binary_id))
   end
 end
