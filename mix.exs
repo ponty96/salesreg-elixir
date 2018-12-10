25e25267fdbc33caca3a7d6b3e3efb25d978c425
Opeyemi Badmos <opeyemi.badmos@gmail.com>
2018-12-10 20:42:55 +0100

    add ip whitelist plug as dependency

--- mix.exs
+++ mix.exs
@@ -69,7 +69,8 @@ defmodule SalesReg.Mixfile do
       {:jason, "~> 1.1"},
       {:arc, "~> 0.11.0"},
       {:pdf_generator, "~> 0.4.0"},
-      {:plug_cowboy, "~> 1.0"}
+      {:plug_cowboy, "~> 1.0"},
+      {:plug_ip_whitelist, "~> 1.0.0"}
     ]
   end
 
