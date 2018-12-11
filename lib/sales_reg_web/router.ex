4a1eac5cc4fd3df9f684610926f2b4eda33c8223
Opeyemi Badmos <opeyemi.badmos@gmail.com>
2018-12-11 15:03:04 +0100

    ensure unpermitted proxies do not gain access to apis

--- lib/sales_reg_web/router.ex
+++ lib/sales_reg_web/router.ex
@@ -11,15 +11,9 @@ defmodule SalesRegWeb.Router do
 
   # Whitelist IPs simply by adding it to the list below
   pipeline :api do
+    plug RemoteIp
+    plug SalesRegWeb.PlugAttack
     plug(:accepts, ["json"])
-    plug Plug.IpWhitelist.IpWhitelistEnforcer, [
-      ip_whitelist: [
-          {52,31,139,75}, 
-          {52,49,173,169},
-          {52,214,14,220}
-      ],
-      response_code_when_blacklisted: 401
-    ]
   end
 
   scope "/api/paystack", SalesRegWeb do
@@ -41,6 +35,6 @@ defmodule SalesRegWeb.Router do
 
   pipe_through([:api, :graphql])
   forward("/graphiql", Absinthe.Plug.GraphiQL, schema: SalesRegWeb.GraphQL.Schemas)
-
+  
   # graphiql endpoint
 end
