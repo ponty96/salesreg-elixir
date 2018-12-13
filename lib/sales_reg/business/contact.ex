70db374441d89c1757b5cf2720659e3813f051fd
kike907 <kikelomofunso@gmail.com>
2018-12-13 18:20:06 +0100

    made changes to contact delete changeset

--- lib/sales_reg/business/contact.ex
+++ lib/sales_reg/business/contact.ex
@@ -75,7 +75,7 @@ defmodule SalesReg.Business.Contact do
   def delete_changeset(contact) do
     contact
     |> Repo.preload([:address, :phone])
-    |> cast(attrs,[])
+    |> cast(%{},[])
     |> assoc_constraint(:company)
     |> assoc_constraint(:user)
     |> cast_assoc(:address)
