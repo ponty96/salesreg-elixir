eead9f3504e2f98f56014e014e278e7a29d53065
Opeyemi Badmos <opeyemi.badmos@gmail.com>
2018-12-13 08:54:31 +0100

    setup configuration for ueberauth and ueberauth_identity

--- config/config.exs
+++ config/config.exs
@@ -71,6 +71,19 @@ config :pdf_generator,
   wkhtml_path: "/usr/local/bin/wkhtmltopdf",
   pdftk_path: "/path/to/pdftk"
 
+config :ueberauth, Ueberauth,
+  providers: [
+    identity: { Ueberauth.Strategy.Identity, [
+        callback_methods: ["POST"],
+        uid_field: :email,
+        nickname_field: :email,
+        request_path: "/login/identity",
+        callback_path: "/login/identity/callback"
+      ]
+    }
+  ]
+
+
 ###############################################################
 ### AWS (image upload functionality) config
 ###############################################################
