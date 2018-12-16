343866e41f43d5b866df7c9038abf2e85dbce2f4
Ayomide Aregbede <ayo.aregbede@gmail.com>
2018-12-16 11:47:15 +0100

    comment out unnecessary seed data

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -57,42 +57,42 @@ customers =
     customer
   end)
 
-vendors =
-  Enum.map(1..25, fn _index ->
-    {:ok, vendor} = Seed.add_contact(user.id, company.id, "vendor")
-    vendor
-  end)
+# vendors =
+#   Enum.map(1..25, fn _index ->
+#     {:ok, vendor} = Seed.add_contact(user.id, company.id, "vendor")
+#     vendor
+#   end)
 
 Enum.map(1..20, fn _index ->
   Seed.add_expense(user.id, company.id)
 end)
 
-templates = 
-  Enum.map(1..5, fn _index ->
-    {:ok, templates} = Seed.add_template()
-    templates
-  end)
+# templates =
+#   Enum.map(1..5, fn _index ->
+#     {:ok, templates} = Seed.add_template()
+#     templates
+#   end)
 
-Seed.add_company_template(company.id, user.id, template.id)
+# Seed.add_company_template(company.id, user.id, template.id)
 
-branch =
-  Repo.all(Branch)
-  |> Enum.random()
+# branch =
+#   Repo.all(Branch)
+#   |> Enum.random()
 
-random_vendors = Enum.take_random(vendors, 8)
-random_customers = Enum.take_random(customers, 8)
+# random_vendors = Enum.take_random(vendors, 8)
+# random_customers = Enum.take_random(customers, 8)
 
-# Enum.map(random_customers, fn customer ->
-#   Seed.create_sales_order(company.id, user.id, customer.id, %{items: products, type: "product"})
-# end)
-
-Enum.map(random_customers, fn customer ->
-  Seed.create_sales_order(company.id, user.id, customer.id, %{items: services, type: "service"})
-end)
+# # Enum.map(random_customers, fn customer ->
+# #   Seed.create_sales_order(company.id, user.id, customer.id, %{items: products, type: "product"})
+# # end)
 
 # Enum.map(random_customers, fn customer ->
-#   Seed.create_sales_order(company.id, user.id, customer.id, products, services)
+#   Seed.create_sales_order(company.id, user.id, customer.id, %{items: services, type: "service"})
 # end)
+
+# # Enum.map(random_customers, fn customer ->
+# #   Seed.create_sales_order(company.id, user.id, customer.id, products, services)
+# # end)
 
 Enum.map(1..10, fn _index ->
   Seed.create_bank(company.id)
