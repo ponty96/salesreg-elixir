d512670225ec9c04beff3bcb1ef7110649f47968
Opeyemi Badmos <opeyemi.badmos@gmail.com>
2018-12-31 18:57:53 +0100

    cherry-pick 03bd0be0b339dc5a82f7594e3c4896a7484654b7 from JIRA-ISSUE-YIWEBP-37

--- lib/sales_reg/mailer/email.ex
+++ lib/sales_reg/mailer/email.ex
@@ -25,77 +25,76 @@ defmodule SalesReg.Email do
       from: email_params.from,
       to: email_params.to,
       subject: email_params.subject,
-      html_body: email_params.html_body
-    )
-    |> Mailer.deliver_later()
-  end
+			html_body: email_params.html_body
+		)
+		|> Mailer.deliver_later()
+	end
 
-  defp transform_template(template, %Sale{} = sale) do
-    EEx.eval_string(template.body, sale: sale)
-  end
+	defp transform_template(template, %Sale{} = sale) do
+		EEx.eval_string(template.body, sale: sale)
+	end
 
-  defp transform_template(binary, %Company{} = company) do
-    EEx.eval_string(binary, company: company)
-  end
+	defp transform_template(binary, %Company{} = company) do
+		EEx.eval_string(binary, company: company)
+	end
+	
+	defp construct_email(html_body, %Company{} = company, type) do
+		%{
+			to: company.contact_email,
+			from: "opeyemi.badmos@yipcart.com",
+			subject: gen_sub(type),
+			html_body: html_body,
+		}
+	end
 
-  defp construct_email(html_body, %Company{} = company, type) do
-    %{
-      to: company.contact_email,
-      from: "opeyemi.badmos@yipcart.com",
-      subject: gen_sub(type),
-      html_body: html_body
-    }
-  end
+	defp construct_email(html_body, %Sale{} = sale, type) do
+		%{
+			to: sale.contact.email,
+			from: sale.company.contact_email, 
+			subject: gen_sub(type),
+			html_body: html_body,
+		}
+	end
 
-  defp construct_email(html_body, %Sale{} = sale, type) do
-    %{
-      to: sale.contact.email,
-      from: sale.company.contact_email,
-      subject: gen_sub(type),
-      html_body: html_body
-    }
-  end
+	defp gen_sub(type) do
+		case type do
+			"yc_email_before_due" ->
+				"New Invoice Created"
+			
+			"yc_email_early_due" ->
+				"Order Payment reminder"
+			
+			"yc_email_late_overdue" ->
+				"Order Payement Overdue"
+			
+			"yc_email_received_order" ->
+				"Received Order"
+			
+			"yc_email_reminder" ->
+				"Payment Reminder"
+		
+			"yc_email_restock" -> 
+				"Restock"
+	
+			"yc_email_welcome_to_yc" -> 
+				"Welcome To YipCart"
 
-  defp gen_sub(type) do
-    case type do
-      "yc_email_before_due" ->
-        "Email Before Due Date"
+			"yc_payment_received" ->
+				"Payment Received"
 
-      "yc_email_early_due" ->
-        "Email Early Due Date"
+			"yc_email_delivered_order" ->
+				"Delivered Order"
 
-      "yc_email_late_overdue" ->
-        "Email Received Overdue"
+			"yc_email_delivering_order" ->
+				"Delivering Order"
 
-      "yc_email_received_order" ->
-        "Email Received Order"
+			"yc_email_pending_delivery" ->
+				"Pending Delivery"
 
-      "yc_email_reminder" ->
-        "Email Reminder"
+			"yc_email_pending_order" ->
+				"Pending Order"
 
-      "yc_email_restock" ->
-        "Email Restock"
-
-      "yc_email_welcome_to_yc" ->
-        "Email Welcome To YC"
-
-      "yc_payment_received" ->
-        "Payment Received"
-
-      "yc_email_delivered_order" ->
-        "Delivered Order"
-
-      "yc_email_delivering_order" ->
-        "Delivering Order"
-
-      "yc_email_pending_delivery" ->
-        "Pending Delivery"
-
-      "yc_email_pending_order" ->
-        "Pending Order"
-
-      _ ->
-        "No Subject"
-    end
-  end
+			_ -> "No Subject"
+		end
+	end
 end
