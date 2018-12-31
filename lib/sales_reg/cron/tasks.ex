c87b989fa17e26a22acc906cba8e6521dc862b73
Opeyemi Badmos <opeyemi.badmos@gmail.com>
2018-12-31 18:57:53 +0100

    cherry-pick 16933f9f66c1e2cafe7f1606b9a97d2ee85d1d63 from JIRA-ISSUE-YIWEBP-37

--- lib/sales_reg/cron/tasks.ex
+++ lib/sales_reg/cron/tasks.ex
@@ -1,40 +1,53 @@
 defmodule SalesReg.Tasks do
   use SalesRegWeb, :context
 
-  def mail_on_order_due_date() do
-    Order.all_invoice()
-    |> Enum.filter(fn invoice ->
-      {:ok, due_date} = Timex.parse(invoice.due_date, "{YYYY}-{0M}-{D}")
-      Date.diff(due_date, now()) == 0
-    end)
-    |> send_mul_email("yc_email_early_due")
-  end
+	# sends emails on the day orders are due for payment
+	def mail_on_order_due_date() do
+		Order.all_invoice()
+		|> Enum.filter(fn invoice ->
+				{:ok, due_date} = Timex.parse(invoice.due_date, "{YYYY}-{0M}-{D}")
+				Date.diff(due_date, now()) == 0
+		end)
+		|> send_mul_email("yc_email_reminder")
+	end
 
-  def mail_before_order_due_date() do
-    Order.all_invoice()
-    |> Enum.filter(fn invoice ->
-      {:ok, due_date} = Timex.parse(invoice.due_date, "{YYYY}-{0M}-{D}")
-      Date.diff(due_date, now()) == 3
-    end)
-    |> send_mul_email("yc_email_before_due")
-  end
+	# sends emails 3 days before the orders are due for payment
+	def mail_before_order_due_date() do
+		Order.all_invoice()
+		|> Enum.filter(fn invoice ->
+				{:ok, due_date} = Timex.parse(invoice.due_date, "{YYYY}-{0M}-{D}") 
+				Date.diff(due_date,  now()) == 3
+		end)
+		|> send_mul_email("yc_email_reminder")
+	end
 
-  def mail_after_order_overdue() do
-    Order.all_invoice()
-    |> Enum.filter(fn invoice ->
-      {:ok, due_date} = Timex.parse(invoice.due_date, "{YYYY}-{0M}-{D}")
-      Date.diff(now, due_date) == 7
-    end)
-    |> send_mul_email("yc_email_late_overdue")
-  end
+	# sends emails 3 days after the orders are due for payment
+	def mail_after_order_due_date() do
+		Order.all_invoice()
+		|> Enum.filter(fn invoice ->
+				{:ok, due_date} = Timex.parse(invoice.due_date, "{YYYY}-{0M}-{D}") 
+				Date.diff(now(), due_date) == 3
+		end)
+		|> send_mul_email("yc_email_early_due")
+	end
 
-  ### Private Functions
-  defp send_mul_email(invoices, type) do
-    Enum.map(invoices, fn invoice ->
-      invoice = Order.preload_invoice(invoice)
-      Email.send_email(type, invoice.sale)
-    end)
-  end
+	# sends emails 7 days after the orders are due for payment
+	def mail_after_order_overdue() do
+		Order.all_invoice()
+		|> Enum.filter(fn invoice ->
+				{:ok, due_date} = Timex.parse(invoice.due_date, "{YYYY}-{0M}-{D}") 
+				Date.diff(now(), due_date) == 7
+		end)
+		|> send_mul_email("yc_email_late_overdue")
+	end
+
+	### Private Functions
+	defp send_mul_email(invoices, type) do
+		Enum.map(invoices, fn(invoice) ->
+				invoice = Order.preload_invoice(invoice)
+				Email.send_email(invoice.sale, type)
+		end)
+	end
 
   defp now() do
     DateTime.to_naive(Timex.now())
