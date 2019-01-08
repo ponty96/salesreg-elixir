55f474f86ccc44007c3e0107194d2697f6db3820
Opeyemi Badmos <opeyemi.badmos@gmail.com>
2019-01-08 20:26:50 +0100

    create a cron job to persist actitivities when the order is due for payment

--- lib/sales_reg/cron/tasks.ex
+++ lib/sales_reg/cron/tasks.ex
@@ -23,9 +23,18 @@ defmodule SalesReg.Tasks do
     Order.all_invoice()
     |> Enum.filter(fn invoice ->
       {:ok, due_date} = Timex.parse(invoice.due_date, "{YYYY}-{0M}-{D}")
-      Date.diff(now, due_date) == 7
+      Date.diff(now(), due_date) == 7
     end)
     |> send_mul_email("yc_email_late_overdue")
+  end
+
+  def create_activity_when_order_due() do
+    Order.all_invoice()
+    |> Enum.filter(fn invoice ->
+      {:ok, due_date} = Timex.parse(invoice.due_date, "{YYYY}-{0M}-{D}")
+      Date.diff(due_date, now()) == 0
+    end)
+    |> create_mul_activities()
   end
 
   ### Private Functions
@@ -33,6 +42,19 @@ defmodule SalesReg.Tasks do
     Enum.map(invoices, fn invoice ->
       invoice = Order.preload_invoice(invoice)
       Email.send_email(type, invoice.sale)
+    end)
+  end
+
+  defp create_mul_activities(invoices) do
+    Enum.map(invoices, fn invoice ->
+      invoice = Order.preload_invoice(invoice)
+      Order.create_activity(
+        "due_payment",
+        "#{Order.calc_pay_outstanding(invoice.sale)}",
+        invoice.id,
+        invoice.sale.contact_id,
+        invoice.company_id
+      )
     end)
   end
 
