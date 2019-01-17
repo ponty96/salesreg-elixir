ee121f14c661a48cbc9144a4d8a6fbcf320968f9
kike907 <kikelomofunso@gmail.com>
2019-01-17 15:00:28 +0100

    create new migration files for review and star to add company_id

---
+++ priv/repo/migrations/20190117135447_add_company_id_to_star.exs
@@ -0,0 +1,11 @@
+defmodule SalesReg.Repo.Migrations.AddCompanyIdToStar do
+  use Ecto.Migration
+
+  def change do
+    alter table(:tables) do
+      add(:company_id, references(:companies, type: :binary_id))
+    end
+
+    create(index(:stars, [:company_id], on_delete: :nothing, type: :binary_id))
+  end
+end
