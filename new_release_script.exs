657c52bf696b58e66552f07221b0417393266707
Opeyemi Badmos <opeyemi.badmos@gmail.com>
2019-01-18 03:07:36 +0100

    add script that runs each time app dyno restarts

---
+++ new_release_script.exs
@@ -0,0 +1,17 @@
+alias SalesReg.{Repo, Theme, Theme.CompanyEmailTemplate}
+
+email_templates = Repo.all(
+  CompanyEmailTemplate
+)
+
+Enum.map(email_templates, fn temp ->
+  type = temp.type
+
+  {:ok, binary} =
+      "./lib/sales_reg_web/templates/mailer/#{type}" <> ".html.eex"
+      |> Path.expand()
+      |> File.read()
+
+  temp
+  |> Theme.update_company_email_template(%{body: binary})
+end)
