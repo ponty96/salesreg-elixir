4c6b2d8fe1a39eb20cfbe2a95cd9ddf38d1d0bf3
Ayomide Aregbede <ayo.aregbede@gmail.com>
2019-02-03 20:00:06 +0100

    fix node errors

--- config/dev.exs
+++ config/dev.exs
@@ -11,7 +11,7 @@ config :sales_reg, SalesRegWeb.Endpoint,
   debug_errors: true,
   code_reloader: true,
   check_origin: false,
-  watchers: [npm: ["run", "watch", cd: Path.expand("../assets", __DIR__)]]
+  watchers: []
 
 # ## SSL Support
 #
