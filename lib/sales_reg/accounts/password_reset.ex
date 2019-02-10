4fe7b2c058dbfbf6595fb5e62fbfd21b684bb155
Opeyemi Badmos <opeyemi.badmos@gmail.com>
2019-02-10 15:22:59 +0100

    create password reset schema and migration file

---
+++ lib/sales_reg/accounts/password_reset.ex
@@ -0,0 +1,25 @@
+defmodule SalesReg.Accounts.PasswordReset do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  @primary_key {:id, :binary_id, autogenerate: true}
+  @foreign_key_type :binary_id
+
+  schema "password_reset" do
+    belongs_to(:users, SalesReg.Accounts.User)
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(password_reset, attrs) do
+    password_reset
+    |> cast(attrs, :user_id)
+    |> validate_required(:user_id)
+  end
+
+  def delete_changeset(password_reset, attrs) do
+    password_reset
+    |> cast(%{}, [])
+  end
+end
