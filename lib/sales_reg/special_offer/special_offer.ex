fdb904bf0d3ea2055a70729b427d64aa53de861e
Aregbede Ayomide <aregbs85@gmail.com>
2019-02-18 16:48:51 +0100

    Merge Develp (#149)
    * adjust hero banner height (#122)
    
    * Product share link (#123)
    
    * mix format
    
    * add/gen product slug
    
    * move get_product_name to Product Schema
    
    * rearrange functions within the sales_reg business context
    
    * add share_link field and resolver to product graphql object
    
    * run mix format on .exs files
    
    * slight changes to product slug and implement category slug
    
    * implement short url redirect to template url
    
    * add option value query params to product redirect url and product page query use slug instead of id
    
    * use slug for categories page query
    
    * company product remove space in option value when creating slug
    
    * remove slug from categories validatio
    
    * Subaccounts (#129)
    
    * implement and integrate logic to create business subaccoounts
    
    * remove sorting paginated company bank lists by is_primary field and edit migration file
    
    * fix error in subaccounts creation (On Gad's system)
    
    * add subaccount_transac_id field to bank and change structure of error response
    
    * change response error format
    
    * change logger debug message
    
    * add charge to sale and get it from the environment
    
    * add CHARGE as environment variable
    
    * change heroku and cloudflare response in development
    
    * Validate emails (#130)
    
    * add local adapter to config file and sentEmailPlug to router
    
    * re-implement sending of emails
    
    * Fix seed data error
    
    * remove uploading of pdf from flow
    
    * fix error in email templates
    
    * Order invoice receipt webstore (#131)
    
    * webstore schema and resolver for order, invoice and receipt
    
    * change :string to :uuid
    
    * change dataloader receipt to receipts
    
    * mix format
    
    * add charge to graphql company schema
    
    * invoice share link
    
    * rearrange types ordering
    
    * properly calculate sale amount considering charge and also calculate + add graphql field for amount before charge
    
    * mix format and add share link to sale, and receipt
    
    * url forwarding for sale and receipt
    
    * add invoice and sale link to email templates
    
    * change email footer
    
    * change how flutterwave key is gotten from environment variables
    
    * fix bug in restock and update product
    
    * Properly preload company branches and location
    
    * fix product slug update bug that arises when user updates product group option values
    
    * load product group
    
    * change Business.get_company_share_url to SalesReg.Business.get_company_share_url
    
    * create initial setup script
    
    * restricting adding of charge and ref_id fields to only changeset insert action
    
    * move Bamboo LocalAdapter from config.exs to dev.exs
    
    * change logic in before_update_callback/2
    
    * make question mark the default option name (#136)
    
    * remove .env from version control
    
    * remove service charge from order amount (#138)
    
    * ensure rave api url is passed from env (#141)
    
    * disallow slugs with underscore (#144)
    
    * disallow slugs with underscore
    
    * add proxied field to create dns record details
    
    * Fix sale order (#148)
    
    * remove payment from sale order
    
    * add invoice allows_split_payment
    
    * implement company bonanza (#147)
    
    * implement company bonanza
    
    * format codebase
    
    * add updated_at field to all graphql objects
    
    * make slug field in bonanza optional and move get bonanza endpoint into webstore graphql schema
    
    * move get bonanza resolver into webstore graphql resolver
    
    * add dataloader for special offer in schema
    
    * add relationship between bonanza and bonanza items
    
    * create bonanza_id index in bonanza_items
    
    * add cast assoc for bonanza_items in bonanza
    
    * Make changes to bonanza crud

---
+++ lib/sales_reg/special_offer/special_offer.ex
@@ -0,0 +1,40 @@
+defmodule SalesReg.SpecialOffer do
+  @moduledoc """
+  The Special Offer context.
+  """
+  use SalesRegWeb, :context
+  alias Dataloader.Ecto, as: DataloaderEcto
+
+  use SalesReg.Context, [
+    Bonanza,
+    BonanzaItem
+  ]
+
+  def data do
+    DataloaderEcto.new(Repo, query: &query/2)
+  end
+
+  def query(queryable, _) do
+    queryable
+  end
+
+  def get_bonanza_if_not_expired(id) do
+    bonanza = Repo.get(Bonanza, id)
+
+    if bonanza_expired?(bonanza) == false do
+      {:ok, bonanza}
+    else
+      {:ok, %{}}
+    end
+  end
+
+  defp bonanza_expired?(bonanza) do
+    {:ok, end_date} = Timex.parse(bonanza.end_date, "{YYYY}-{0M}-{D}")
+
+    if Date.compare(Date.utc_today(), end_date) == :gt do
+      true
+    else
+      false
+    end
+  end
+end
