29daadbc5f34b36dafe151be6c4fe96c486f4090
Opeyemi Badmos <opeyemi.badmos@gmail.com>
2019-02-27 01:47:01 +0100

    fix association error in notification object type

--- lib/sales_reg_web/graphql/types.ex
+++ lib/sales_reg_web/graphql/types.ex
@@ -645,7 +645,7 @@ defmodule SalesRegWeb.GraphQL.DataTypes do
     field(:actor, :user, resolve: dataloader(SalesReg.Accounts, :actor))
 
     field(:notification_items, list_of(:notification_item),
-      resolve: dataloader(SalesReg.Notifications, :notification_item)
+      resolve: dataloader(SalesReg.Notifications, :notification_items)
     )
   end
 
