99384bc9c9aef4b5c795dd406003afb87c0258af
Badmos Opeyemi Philip <opeyemi.badmos@gmail.com>
2019-03-11 13:44:15 +0100

    Delivery fee (#156)
    * implement company delivery fee
    
    * format codebase
    
    * get sentry dsn from the environment
    
    * reimplement delivery fee
    
    * fix typos
    
    * query to check if company already has a delivery fee for nation wide deliveries
    
    * add delivery fee to sale
    
    * add delivery fee to sale
    
    * add field that company delivers nationwide
    
    * add delivery_fee to sale and also to sale amount
    
    * fix bug in list company delivery fees

---
+++ lib/sales_reg/order/delivery_fee.ex
@@ -0,0 +1,44 @@
+defmodule SalesReg.Order.DeliveryFee do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  @primary_key {:id, :binary_id, autogenerate: true}
+  @foreign_key_type :binary_id
+
+  schema "delivery_fees" do
+    field(:fee, :string)
+    field(:state, :string)
+    field(:region, :string)
+
+    belongs_to(:company, SalesReg.Business.Company)
+    belongs_to(:user, SalesReg.Accounts.User)
+
+    timestamps()
+  end
+
+  @required_fields [
+    :fee,
+    :state,
+    :region,
+    :user_id,
+    :company_id
+  ]
+  @optional_fields []
+
+  def changeset(delivery_fee, attrs) do
+    delivery_fee
+    |> cast(attrs, @required_fields ++ @optional_fields)
+    |> validate_required(@required_fields)
+    |> assoc_constraint(:user)
+    |> assoc_constraint(:company)
+    |> unique_constraint(:region,
+      name: :state_region_index,
+      message: "This region has already been taken for this state."
+    )
+  end
+
+  def delete_changeset(delivery_fee) do
+    delivery_fee
+    |> cast(%{}, [])
+  end
+end
