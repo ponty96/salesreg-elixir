a7a57c83abdb6ed38b95362503be246492765972
Ayomide Aregbede <ayo.aregbede@gmail.com>
2019-03-31 01:36:04 +0100

    fix tslint errors

--- assets/js/socket.ts
+++ assets/js/socket.ts
@@ -57,9 +57,9 @@ const socket = new Socket('/socket', {
 socket.connect()
 
 // Now that you are connected, you can join channels with a topic:
-let channel = socket.channel("topic:subtopic", {})
+const channel = socket.channel("topic:subtopic", {})
 channel.join()
-  .receive("ok", resp => { console.log("Joined successfully", resp) })
-  .receive("error", resp => { console.log("Unable to join", resp) })
+  .receive("ok", (resp: any) => { console.log("Joined successfully", resp) })
+  .receive("error", (resp: any) => { console.log("Unable to join", resp) })
 
 export default socket
