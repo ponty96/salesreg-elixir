cd25113089fb7bc1ebaa644ce16cf8aa06875629
Ayomide Aregbede <ayo.aregbede@gmail.com>
2019-03-31 15:42:40 +0100

    setup basic page

--- assets/js/app.tsx
+++ assets/js/app.tsx
@@ -1,35 +1,17 @@
-// Brunch automatically concatenates all files in your
-// watched paths. Those paths can be configured at
-// config.paths.watched in "brunch-config.js".
-//
-// However, those files will only be executed if
-// explicitly imported. The only exception are files
-// in vendor, which are never wrapped in imports and
-// therefore are always executed.
-
-// Import dependencies
-//
-// If you no longer want to use a dependency, remember
-// to also remove its path from "config.paths.watched".
 import 'phoenix_html';
-
-// Import local files
-//
-// Local files can be imported directly using relative
-// paths "./socket" or full ones "web/static/js/socket".
-
-// import socket from "./socket"
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import HomePage from '@/pages/HomePage';
 
-class HelloMessage extends React.Component {
+class App extends React.Component {
 	render() {
 		return (
-			<React.Fragment>
-				<h1>Hello</h1>
-			</React.Fragment>
+			<Router>
+				<Route path="/" exact component={HomePage} />
+			</Router>
 		);
 	}
 }
 
-ReactDOM.render(<HelloMessage />, document.getElementById('body'));
+ReactDOM.render(<App />, document.getElementById('body'));
---
+++ assets/js/pages/HomePage.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+
+export default class HomePage extends React.Component<any, any> {
+	render() {
+		return <div className="btn btn-primary">Home page ddddd</div>;
+	}
+}
