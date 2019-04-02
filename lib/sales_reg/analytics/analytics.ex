9e4716848575acbe4f8fc6439d916845f6f4e43b
Ayomide Aregbede <ayo.aregbede@gmail.com>
2019-04-02 18:45:45 +0100

    rearrange functions in analytics module

--- lib/sales_reg/analytics/analytics.ex
+++ lib/sales_reg/analytics/analytics.ex
@@ -12,29 +12,9 @@ defmodule SalesReg.Analytics do
       |> Repo.aggregate(:sum, :total_amount)
       |> to_float()
 
-    top_expenses =
-      Repo.all(
-        from(e in query,
-          select: merge(map(e, ^[:title]), %{total_in_group: sum(e.total_amount)}),
-          group_by: e.title,
-          order_by: [desc: sum(e.total_amount)],
-          limit: 5
-        )
-      )
+    data_points = expense_data_points(query, group_by)
 
-    data_points =
-      Repo.all(
-        from(e in query,
-          select: %{
-            total: sum(e.total_amount),
-            date: min(e.date)
-          },
-          group_by: [fragment("date_trunc(?, ?)", ^group_by, e.date)],
-          order_by: [desc: sum(e.total_amount)]
-        )
-      )
-
-    top_expenses = top_expenses |> convert_decimal_items_to_float(:total_in_group)
+    top_expenses = get_top_expenses(query)
 
     {:ok,
      %{
@@ -48,28 +28,9 @@ defmodule SalesReg.Analytics do
   def dashboard_info(:order, start_date, end_date, group_by, company_id) do
     query = schemas_within_range(Sale, start_date, end_date, company_id)
 
-    order_statuses =
-      Repo.all(
-        from(s in query,
-          select: %{
-            status: s.status,
-            count: count(s.status)
-          },
-          group_by: s.status
-        )
-      )
+    order_statuses = get_order_statuses(query)
 
-    data_points =
-      Repo.all(
-        from(e in query,
-          select: %{
-            total: count(e.id),
-            date: min(e.date)
-          },
-          group_by: [fragment("date_trunc(?, ?)", ^group_by, e.date)],
-          order_by: [desc: count(e.id)]
-        )
-      )
+    data_points = order_data_points(query, group_by)
 
     {:ok,
      %{
@@ -91,38 +52,13 @@ defmodule SalesReg.Analytics do
 
     items_query = income_items_query(start_date, end_date, company_id)
 
-    amount_due_query =
-      from(i in items_query,
-        select: sum(i.quantity * i.unit_price)
-      )
-
-    amount_due =
-      amount_due_query
-      |> Repo.all()
-      |> Enum.at(0)
+    amount_due = income_amount_due(items_query)
 
     total_products = Repo.aggregate(items_query, :sum, :quantity)
 
-    top_products =
-      Repo.all(
-        from(i in items_query,
-          select: %{product_id: i.product_id, title: "", amount: sum(i.quantity * i.unit_price)},
-          group_by: i.product_id,
-          order_by: [desc: sum(i.quantity * i.unit_price)]
-        )
-      )
+    top_products = income_top_products(items_query)
 
-    data_points =
-      Repo.all(
-        from(inc in incomes_in_range_query,
-          select: %{
-            total: sum(inc.amount_paid),
-            date: min(inc.time_paid)
-          },
-          group_by: [fragment("date_trunc(?, ?)", ^group_by, inc.time_paid)],
-          order_by: [desc: count(inc.amount_paid)]
-        )
-      )
+    data_points = income_data_points(incomes_in_range_query, group_by)
 
     {:ok,
      %{
@@ -132,6 +68,92 @@ defmodule SalesReg.Analytics do
        amount_due: amount_due,
        data_points: data_points
      }}
+  end
+
+  defp expense_data_points(query, group_by) do
+    Repo.all(
+      from(e in query,
+        select: %{
+          total: sum(e.total_amount),
+          date: min(e.date)
+        },
+        group_by: [fragment("date_trunc(?, ?)", ^group_by, e.date)],
+        order_by: [desc: sum(e.total_amount)]
+      )
+    )
+  end
+
+  defp get_top_expenses(query) do
+    top_expenses =
+      Repo.all(
+        from(e in query,
+          select: merge(map(e, ^[:title]), %{total_in_group: sum(e.total_amount)}),
+          group_by: e.title,
+          order_by: [desc: sum(e.total_amount)],
+          limit: 5
+        )
+      )
+
+    top_expenses |> convert_decimal_items_to_float(:total_in_group)
+  end
+
+  defp get_order_statuses(query) do
+    Repo.all(
+      from(s in query,
+        select: %{
+          status: s.status,
+          count: count(s.status)
+        },
+        group_by: s.status
+      )
+    )
+  end
+
+  defp order_data_points(query, group_by) do
+    Repo.all(
+      from(e in query,
+        select: %{
+          total: count(e.id),
+          date: min(e.date)
+        },
+        group_by: [fragment("date_trunc(?, ?)", ^group_by, e.date)],
+        order_by: [desc: count(e.id)]
+      )
+    )
+  end
+
+  defp income_top_products(items_query) do
+    Repo.all(
+      from(i in items_query,
+        select: %{product_id: i.product_id, title: "", amount: sum(i.quantity * i.unit_price)},
+        group_by: i.product_id,
+        order_by: [desc: sum(i.quantity * i.unit_price)]
+      )
+    )
+  end
+
+  defp income_amount_due(items_query) do
+    amount_due_query =
+      from(i in items_query,
+        select: sum(i.quantity * i.unit_price)
+      )
+
+    amount_due_query
+    |> Repo.all()
+    |> Enum.at(0)
+  end
+
+  defp income_data_points(incomes_in_range_query, group_by) do
+    Repo.all(
+      from(inc in incomes_in_range_query,
+        select: %{
+          total: sum(inc.amount_paid),
+          date: min(inc.time_paid)
+        },
+        group_by: [fragment("date_trunc(?, ?)", ^group_by, inc.time_paid)],
+        order_by: [desc: count(inc.amount_paid)]
+      )
+    )
   end
 
   defp income_items_query(start_date, end_date, company_id) do
